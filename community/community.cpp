1f92a207fcaf3ac569868604ba577d2f367f9dd1
박성민 <sm136599@gmail.com>
2024-08-03 22:23:44 +0900

    refactor: edit community base code

--- community/community.cpp
+++ community/community.cpp
@@ -6,4 +6,26 @@ using namespace std;
 
 #include "community.h"
 
-// 구현
+Community::Community() {
+    cohort = "";
+    group = "";
+}
+
+void Community::loadStudents() {
+
+}
+// 랜덤 파티 생성
+void Community::makeParty() {
+
+}
+// 파티 저장
+void Community::saveParty() {
+
+}
+
+string Community::getCohort() const {
+    return "";
+}
+string Community::getGroup() const {
+    return "";
+}
