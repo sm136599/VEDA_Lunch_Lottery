f8930375c72e2a824cc866b669e7a51abf632614
U-keun Song <sukeun319@gmail.com>
2024-08-03 13:55:09 +0900

    fix : 입력 기능 수정
    파일에 데이터가 한 줄로 이루어져 있다는 가정을 없애서 여러 줄의 입력을 받아서 2차원 vector를 가리키는 유니크 포인터를 반환하는 함수로 수정했습니다.

--- utility/fileIO.h
+++ utility/fileIO.h
@@ -5,6 +5,7 @@
 #define FILEIO_H
 
 #include <fstream>
+#include <vector>
 #include <string>
 #include <stdexcept>
 #include <memory>
@@ -13,28 +14,31 @@
 using namespace std;
 
 template <class T>
-unique_ptr< vector<T> > readFile(string filePath, char delimiter) {
+unique_ptr< vector< vector<T> > > readFile(string filePath, char delimiter) {
     ifstream fin;
     fin.open(filePath);
 
     if (!fin) throw runtime_error("There is no such file.\n");
 
-    auto result = make_unique< vector<T> >();
+    auto result = make_unique< vector< vector<T> > >();
 
-    // 입력이 한 줄로 되어 있다고 가정
-    string token;
-    while (getline(fin, token, delimiter)) {
-        if constexpr (is_same_v<T, string>) {
-            (*result).push_back(token);
-        } else {
-            stringstream tokenStream(token);
-            T value;
-            tokenStream >> value;
-            if (tokenStream.fail()) {
-                throw runtime_error("Failed to convert token to the specified type.\n");
+    string line, token;
+    int idx = 0;
+    while (getline(fin, line)) {
+        stringstream lineStream(line);
+        vector<T> row;
+        while (getline(lineStream, token, delimiter)) {
+            if constexpr (is_same_v<T, string>) {
+                row.push_back(token);
+            } else {
+                stringstream tokenStream(token);
+                T value;
+                tokenStream >> value;
+                if (tokenStream.fail()) throw runtime_error("failed to convert token to the specified type.\n");
+                row.push_back(value);
             }
-            (*result).push_back(value);
         }
+        result->push_back(row);
     }
 
     fin.close();
