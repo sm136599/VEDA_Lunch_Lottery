8f7ebafabc7b4497abfafe8c5e9c955b374392b2
박성민 <sm136599@gmail.com>
2024-08-04 15:08:07 +0900

    refactor: 화면 표시 내용 변경

--- view/classCreationView.cpp
+++ view/classCreationView.cpp
@@ -2,7 +2,7 @@
 
 void ClassCreationView::display(const Community* comm) {
     string title = "반 생성";
-    vector<string> options = {"기수", "반", "종료"};
+    vector<string> options = {"기수 " + comm->getCohort(), "반 " + comm->getGroup(), "완료"};
     IView::displayMenu(title, options, comm);
 }
 
--- view/classSelectionView.cpp
+++ view/classSelectionView.cpp
@@ -2,6 +2,8 @@
 
 void ClassSelectionView::display(const Community* comm) {
     string title = "반 선택";
+    // vector<string> options = getFileNames("./data");
+    // options.push_back("종료");
     vector<string> options = {"종료"};
     IView::displayMenu(title, options, comm);
 }
