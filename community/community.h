5624769259df2491c607d3ef01ddc1528bb40c68
박성민 <sm136599@gmail.com>
2024-08-05 13:30:23 +0900

    refactor: edit loadParty, makeParty, create getter, setter

--- community/community.h
+++ community/community.h
@@ -9,25 +9,40 @@ using namespace std;
 //#include "../headers.h"
 #include "../utility/fileIO.h"
 
+#define FILEPATH "./data/" + this->cohort + "_" + this->group + ".csv"
+#define DELIMITER ','
+
 class Community {
 private:
     string cohort;  // 기수
-    string group;   // 반  
-    //unique_ptr< vector< vector<string> > > originalData;  //함수에서 직접 선언
-    vector<vector<string>> groupedData;
-    //vector<string> students;            //파일입출력으로 불러올거   
-    //vector<vector<string>> prevParty;   //불러온거를 다시 쪼개서 넣을거?
+    string group;   // 반 
+    vector< vector<string> > party;  
+    vector< vector<string> > nextParty;
+
+    void shiftRight(vector<string>& row, int shift);
 public:
     Community();
     // 파일에서 데이터 불러오기
-    unique_ptr< vector< vector<string> > > loadStudents();
+    void loadStudents();
     // 랜덤 파티 생성
-    vector<vector<string>> makeParty();
+    void makeParty();
     // 파티 저장
     void saveParty();
 
     string getCohort() const;
     string getGroup() const;
+    void setCohort(string cohort) {
+        this->cohort = cohort;
+    };
+    void setGroup(string group) {
+        this->group = group;
+    };
+    vector< vector<string> > getParty() const {
+        return party;
+    }
+    vector< vector<string> > getNextParty() const {
+        return nextParty;
+    }
 };
 
 #endif
