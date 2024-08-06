24874906e9f8b9025d3905aa91b00b8af4ad8a8e
박성민 <sm136599@gmail.com>
2024-08-06 17:58:28 +0900

    feat: add exception process
    - 입력 시 예외처리 추가
    - 파일 저장 로직 추가

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -14,6 +14,17 @@ Controller::Controller(Community* comm) {
     classSelectionView = new ClassSelectionView();
     shuffleView = new ShuffleView();
 }
+bool Controller::getIntCommand(int& cmd) {
+    cout << ">> ";
+    cin >> cmd;
+    if (cin.fail()) {
+        cin.clear();
+        cin.ignore();
+        cout << "다시 입력해주세요." << endl;
+        return false;
+    }
+    return true;
+}
 void Controller::run() {
     setView(mainMenuView);
     while (true) { 
@@ -40,9 +51,9 @@ void Controller::processInput() {
 }
 void Controller::processMainMenu() {
     int cmd;
-    cin >> cmd;
-    fflush(stdin);
-    if (cin.fail()) cout << "다시 입력해주세요" << endl;
+    if (!getIntCommand(cmd)) {
+        return;
+    }
     switch (cmd)
     {
     case 1:
@@ -63,9 +74,9 @@ void Controller::processMainMenu() {
 }
 void Controller::processClassCreation() {
     int cmd;
-    cin >> cmd;
-    fflush(stdin);
-    if (cin.fail()) cout << "다시 입력해주세요" << endl;
+    if (!getIntCommand(cmd)) {
+        return;
+    }
     switch (cmd)
     {
     case 1: {
@@ -86,6 +97,11 @@ void Controller::processClassCreation() {
         }
     case 3: {
             // 기수_반.csv 파일 형식으로 data폴더 안에 저장하기
+            // 기수와 반이 없으면 어떻게 하지?
+            if (community->getCohort() == "" || community->getGroup() == "") {
+                cout << "기수와 반 모두 입력해주세요" << endl;
+                return;
+            }
             string fileName = "./data/" + community->getCohort() + "_" + community->getGroup() + ".csv";
             ofstream fout; 
             fout.open(fileName);
@@ -103,11 +119,16 @@ void Controller::processClassCreation() {
 }
 void Controller::processClassSelection() {
     int cmd;
-    cin >> cmd;
-    fflush(stdin);
-    if (cin.fail()) cout << "다시 입력해주세요" << endl;
+    if (!getIntCommand(cmd)) {
+        return;
+    }
     // 파일 이름: 기수_반.csv
     vector<string> files = readFileNames("./data");
+
+    if (!(1 <= cmd && cmd <= files.size() + 1)) {
+        cout << "번호를 다시 입력해주세요" << endl;
+        return; 
+    }
     if (cmd == files.size() + 1) setView(mainMenuView);
     else {
         // community 멤버 변수에 기수, 반 입력
@@ -119,7 +140,7 @@ void Controller::processClassSelection() {
         community->setCohort(cohort);
         community->setGroup(group);
         community->loadStudents();
-        setView(classSelectionView);
+        setView(mainMenuView);
     }
 
 }
@@ -127,7 +148,7 @@ void Controller::processShuffle() {
     community->makeParty();
     // 만들어진 파티 보여주기
     vector< vector<string> > party = community->getParty();
-    cout << "======지난 주======" << endl;
+    cout << "==========지난 주==========" << endl;
     for (int i = 0;i < party.size(); i++) {
         cout << i + 1 << "조 : ";
         for (const auto& person : party[i]) {
@@ -135,7 +156,7 @@ void Controller::processShuffle() {
         }
         cout << endl;
     }
-    cout << "======이번 주======" << endl;
+    cout << "==========이번 주==========" << endl;
     party = community->getNextParty();
     for (int i = 0;i < party.size(); i++) {
         cout << i + 1 << "조 : ";
@@ -144,6 +165,9 @@ void Controller::processShuffle() {
         }
         cout << endl;
     }
+    cout << "위의 이번 주 조합이 저장됩니다. 지난 주 기록은 사라집니다." << endl;
     pause;
+    writeFile(("./data/" + community->getCohort() + "_" + community->getGroup() + ".csv").c_str(), ',', community->getNextParty());
+    community->loadStudents();
     setView(mainMenuView);
 }
